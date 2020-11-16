1ad73e79bd7752203ed875ea3afd05e91cffcda7
Zachariah Sharma <zachariah.sharma@icloud.com>
2020-11-16 11:05:37 -0500

    fast catch up

---
+++ public/js/settings.js
@@ -0,0 +1,14 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCTbG4Q4dgjviI7yKZQne0IE78W9wk0JeE",
+    authDomain: "sofit-cc1f1.firebaseapp.com",
+    databaseURL: "https://sofit-cc1f1.firebaseio.com",
+    projectId: "sofit-cc1f1",
+    storageBucket: "sofit-cc1f1.appspot.com",
+    messagingSenderId: "641436990806",
+    appId: "1:641436990806:web:5719e87b10df058c676af7",
+    measurementId: "G-LVWFF4W39X"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+firebase.analytics();
+var db = firebase.firestore();
